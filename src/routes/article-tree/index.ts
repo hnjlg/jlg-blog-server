22251925a878e115ae075272ed0cc4ede48adabf
lisp <1501110991@qq.com>
2024-02-02 15:43:24 +0800

    feat: 目录编辑接口

--- src/routes/article-tree/index.ts
+++ src/routes/article-tree/index.ts
@@ -3,10 +3,12 @@ import deleteById from './deleteById';
 import searchByArticleTreeName from './searchByArticleTreeName';
 import add from './add';
 import searchById from './searchById';
+import updateById from './updateById';
 
 export default ({ app }: { app: Application }) => {
 	deleteById({ app });
 	searchByArticleTreeName({ app });
 	add({ app });
 	searchById({ app });
+	updateById({ app });
 };
